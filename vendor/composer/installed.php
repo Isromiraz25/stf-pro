6c63e92709a2318ab36e41bf2fac7f4b0e1b3ad3
isromiraz25 <isromiraz25@gmail>
2024-12-11 11:57:20 +0700

    updatecomposer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bde77bbbb662c4a2011741516df49c0dc1b1b499',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -431,9 +431,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bde77bbbb662c4a2011741516df49c0dc1b1b499',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
